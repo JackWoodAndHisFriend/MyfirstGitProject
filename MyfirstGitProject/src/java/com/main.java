9fd029a47160d792a545a9449b7d921b41eb241a
hantao zhou <zht990207@mail.ustc.edu.cn>
2022-05-30 09:29:16 +0800

    2022-5-30 first commit

---
+++ MyfirstGitProject/src/java/com/main.java
@@ -0,0 +1,17 @@
+package java.com;
+
+import java.util.*;
+/**
+ * Author: Tenno
+ * Date: 2022-05-30 9:26
+ */
+
+public class main {
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        while(scan.hasNext()){
+            System.out.println(scan);
+        }
+    }
+}
