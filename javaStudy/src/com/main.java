bb737a79f6842aace2782876ee7b761355da5675
hantao zhou <zht990207@mail.ustc.edu.cn>
2022-06-01 09:20:59 +0800

    feat : javaStudy

---
+++ javaStudy/src/com/main.java
@@ -0,0 +1,16 @@
+package com;
+
+/**
+ * Author: Tenno
+ * Date: 2022-05-30 11:49
+ */
+
+public class main {
+
+    public static void main(String[] args) {
+        String x = "通话";
+        String y = "重地";
+        System.out.println(String.format("%d,%d",x.hashCode(),y.hashCode()));
+        System.out.println(x.equals(y));
+    }
+}
