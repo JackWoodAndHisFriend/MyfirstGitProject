cb8d1eedb99ef6153527d69d518a7828dda78250
Tenno <1670569304@qq.com>
2022-06-30 15:40:59 +0800

    feat new repo:ps

---
+++ PhotoShop/src/main/java/com/example/photoshop/PhotoShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.photoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoShopApplication.class, args);
+	}
+
+}
---
+++ PhotoShop/src/test/java/com/example/photoshop/PhotoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photoshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
