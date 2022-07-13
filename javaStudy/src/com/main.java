504672184af215323e49650d1acc11a6bb5cd825
Tenno <1670569304@qq.com>
2022-07-13 14:54:10 +0800

    feat idea decode

--- javaStudy/src/com/main.java
+++ javaStudy/src/com/main.java
@@ -5,6 +5,7 @@ package com;
  * Date: 2022-05-30 11:49
  */
 //https://zhuanlan.zhihu.com/p/64147696
+//https://javaguide.cn/java/basis/java-basic-questions-01.html#java-%E8%AF%AD%E8%A8%80%E6%9C%89%E5%93%AA%E4%BA%9B%E7%89%B9%E7%82%B9
 public class main {
 
     public static void main(String[] args) {
@@ -14,3 +15,7 @@ public class main {
         System.out.println(x.equals(y));
     }
 }
+
+
+
+
